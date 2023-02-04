d86ccd01b525fa588cb71890935ed0b390908d37
EijiSambei <sambei13579@gmail.com>
2023-02-04 21:27:27 +0900

    change web3

--- src/web3.js
+++ src/web3.js
@@ -1,7 +1,7 @@
 import Web3 from 'web3'
 var web3 = false;
 
-    if (window.ethereum) {
+    if (window.ethereum !== 'undefined') {
             window.ethereum.request({ method: 'eth_requestAccounts' });
             web3 = new Web3(window.ethereum);
         }
